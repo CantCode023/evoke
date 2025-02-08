62036e690b96b591c8b668aa2a5548f8ba76da87
bd. <cantcode023@gmail.com>
2025-02-09 02:45:11 +0800

    added get_client

---
+++ src/agents/client.py
@@ -0,0 +1,19 @@
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_client():
+    model_client = OpenAIChatCompletionClient(
+        model="gemini-2.0-flash",
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+        api_key=os.environ["GEMINI_API_KEY"],
+        model_info={
+            "vision": True,
+            "function_calling": True,
+            "json_output": True,
+            "family": "unknown"
+        }
+    )
+    
+    return model_client
