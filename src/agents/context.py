e92070ddf9c6f6cd10f874be8c7ccedf47d16f31
bd. <cantcode023@gmail.com>
2025-02-09 20:06:29 +0800

    added random name hash generator to differentiate context agents

--- src/agents/context.py
+++ src/agents/context.py
@@ -5,12 +5,17 @@ from autogen_agentchat.messages import TextMessage
 
 from .client import get_client
 
-def add_context(*tool):
+def generate_hash() -> str:
+    import string, random
+    return "".join(random.choices(string.ascii_lowercase, k=12))
+
+def add_context(*tool, system_message:str|None=None) -> AssistantAgent:
+    hash_name = generate_hash() + "_context_agent"
     context_agent = AssistantAgent(
-        name="context_agent",
+        name=hash_name,
         model_client=get_client(),
         tools=[*tool],
-        system_message="""Use the tool given and return the results."""
+        system_message="""Use the tool given and return the results.""" if system_message is None else system_message,
     )
     
     return context_agent
