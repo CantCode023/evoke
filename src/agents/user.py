9339b5b1d981c8a65ebaebf742b41f3347f6d9df
bd. <cantcode023@gmail.com>
2025-02-09 22:02:54 +0800

    👤 Added UserProxyAgent

--- src/agents/user.py
+++ src/agents/user.py
@@ -1,3 +1,3 @@
 from autogen_agentchat.agents import UserProxyAgent
-
-user_proxy = UserProxyAgent("user_proxy", input_func=lambda _: input("[:] "))
+        
+user_proxy = UserProxyAgent("user_proxy", input_func=input)
