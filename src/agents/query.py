3e34cbecedcfecae7146bc27db40a86c45c28770
bd. <cantcode023@gmail.com>
2025-02-09 13:22:40 +0800

    changed system_message

--- src/agents/query.py
+++ src/agents/query.py
@@ -5,15 +5,13 @@ from autogen_agentchat.messages import TextMessage
 
 from .client import get_client
 
-model_client = get_client()
-
 query_agent = AssistantAgent(
     name="query_agent",
-    model_client=model_client,
+    model_client=get_client(),
     system_message="""
     You are a Github repository search query generator assistant. Based on the user's prompt, you will
-    generate an appropriate query to search for using the Github API. Make sure the query will only return 100
-    repositories by default, this will be changed if the user specifies a different number.
+    generate an appropriate query to search for using the Github API. Make sure the query will only return
+    100 repositories.
     Example:
     user: Give me 10 most starred repositories about machine learning with language of Python.
     assistant: "language:python topic:machine-learning stars:>10000 sort:stars-desc"
