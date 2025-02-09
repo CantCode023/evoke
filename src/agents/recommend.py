8cbdf007e338e5e32f0bba59f1275e88d4cc28e0
bd. <cantcode023@gmail.com>
2025-02-09 22:01:57 +0800

    updated system_message, using reflect_on_tool_use=True to allow tool context reflection instead of pulling tool output which burn tokens

--- src/agents/recommend.py
+++ src/agents/recommend.py
@@ -1,7 +1,4 @@
 from autogen_agentchat.agents import AssistantAgent
-from autogen_agentchat.ui import Console
-from autogen_core import CancellationToken
-from autogen_agentchat.messages import TextMessage
 
 from .client import get_client
 from src.tools.repo import github_repo_search
@@ -9,9 +6,22 @@ from src.tools.repo import github_repo_search
 recommend_agent = AssistantAgent(
     name="recommend_agent",
     model_client=get_client(),
+    tools=[github_repo_search],
     system_message="""
     You are a project recommendation agent. 
+    Use the tool given to find github repositories based on the query given.
     Given the list of github repositories, choose the repositories that best match the user's prompt.
-    Display only the URLs of chosen repositories.
-    """
+    Note that the repositories recommended must not be a repository that works as a package.
+    For example, do not recommend "https://github.com/fastapi/fastapi", but instead recommend repositories that uses fastapi to make a unique project.
+    Display the URLs of chosen repositories and the description below it.
+    If an error occurs, return the error message and then return TERMINATE.
+    Example:
+    - URL
+    > Description
+    (newline)
+    - URL 2
+    > Description 2
+    ...
+    """,
+    reflect_on_tool_use=True
 )
