d85666f267f84260d14256a4184424f0c6489df5
bd. <cantcode023@gmail.com>
2025-02-09 22:01:06 +0800

    updated system_message

--- src/agents/query.py
+++ src/agents/query.py
@@ -1,7 +1,4 @@
 from autogen_agentchat.agents import AssistantAgent
-from autogen_agentchat.ui import Console
-from autogen_core import CancellationToken
-from autogen_agentchat.messages import TextMessage
 
 from .client import get_client
 
@@ -9,9 +6,8 @@ query_agent = AssistantAgent(
     name="query_agent",
     model_client=get_client(),
     system_message="""
-    You are a Github repository search query generator. Based on the user's prompt, you'll
-    generate a query to search in the Github API. Make sure to not limit the results by user's prompt.
-    For example, if the user asks for 10 most starred repos, do not limit the results to 10 repos.
+    You are a Github repository search query generator.
+    Your task is to generate a Github search query based on the user's prompt.
     Example:
     user: Give me 10 most starred repositories about machine learning with language of Python.
     assistant: "language:python topic:machine-learning stars:>10000 sort:stars-desc"
