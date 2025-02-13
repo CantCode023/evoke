f0247def536c127a350a590407dd4bdebd3233ae
bd. <cantcode023@gmail.com>
2025-02-13 18:48:38 +0800

    🔖 Evoke CLI 0.0.1

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 from src import Evoke
+from rich import print as rprint
+from rich.pretty import pprint
+import os
 
 evoke = Evoke()
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
     
-async def main(task: str):
-    await evoke.ask(task)
+async def main():
+    clear()
+    
+    MENU = """[bright_white]:::::::::: :::     :::  ::::::::  :::    ::: :::::::::: 
+:+:        :+:     :+: :+:    :+: :+:   :+:  :+:        
++:+        +:+     +:+ +:+    +:+ +:+  +:+   +:+        
++#++:++#   +#+     +:+ +#+    +:+ +#++:++    +#++:++#   
++#+         +#+   +#+  +#+    +#+ +#+  +#+   +#+        
+#+#          #+#+#+#   #+#    #+# #+#   #+#  #+#        
+##########     ###      ########  ###    ### ##########[/bright_white]
+[bright_black]Evoking excitement..[/bright_black]
+
+type TERMINATE to exit.."""
+
+    rprint(MENU)
+    
+    task = input("[:] ")
+    if task == "TERMINATE":
+        return 0
+    else:
+        result = await evoke.ask(task_message=task)
+        pprint(result)
+        print("Press enter to continue...")
+        input()
+        return 1
     
 if __name__ == "__main__":
     import asyncio
-    task = input("[:] ")
-    asyncio.run(main(task))
+    result = 1
+    while result:
+        result = asyncio.run(main())
