a3c2fe1c23a1b2456c7373fe9929349022aeef81
bd. <cantcode023@gmail.com>
2025-02-18 19:15:27 +0800

    🔥 Removed duplicated code

--- src/agents/client.py
+++ src/agents/client.py
@@ -17,18 +17,3 @@ def get_client():
     )
     
     return model_client
-    
-def get_client_for_report():
-    model_client = OpenAIChatCompletionClient(
-        model="google/gemini-2.0-flash-exp:free",
-        base_url="https://openrouter.ai/api/v1",
-        api_key=os.environ["OPENROUTER_API_KEY"],
-        model_info={
-            "vision": False,
-            "function_calling": True,
-            "json_output": False,
-            "family": "unknown"
-        }
-    )
-    
-    return model_client
